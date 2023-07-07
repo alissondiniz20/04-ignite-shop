23f0f5da2a71d4eb234143b1f55507ddf56c3c1d
alissondiniz20 <alisson.dev22@gmail.com>
2023-07-06 18:30:00 -0600

    stitches config

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,3 +1,25 @@
+import { styled } from "../styles";
+
+const Button = styled("button", {
+  backgroundColor: "$rocketseat",
+  borderRadius: 4,
+  border: 0,
+  padding: "4px 8px",
+
+  span: {
+    fontWeight: "bold",
+  },
+
+  "&:hover": {
+    filter: "brightness(0.8)",
+  },
+});
+
 export default function Sucess() {
-  return <h1>Success</h1>;
+  return (
+    <Button>
+      <span>Testesdsds</span>
+      Enviar
+    </Button>
+  );
 }
