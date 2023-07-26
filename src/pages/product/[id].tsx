6ca23c158c0b47b9b1fbcbb45abb3453276a7700
alissondiniz20 <alisson.dev22@gmail.com>
2023-07-25 20:48:12 -0600

    product page structure

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,30 @@
 import { useRouter } from "next/router";
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from "../../styles/pages/product";
 
 export default function Product() {
   const { query } = useRouter();
 
-  return <h1>Product: {JSON.stringify(query)}</h1>;
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+          nihil ut nisi accusamus quam veritatis, odit labore eius illo
+          dignissimos earum ducimus facere cum, id delectus fugiat error quis
+          quidem.
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  );
 }
