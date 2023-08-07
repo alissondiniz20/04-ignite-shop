b795972b27078aa4025843357b8a57c9b1b51fee
alissondiniz20 <alisson.dev22@gmail.com>
2023-08-07 17:19:23 -0600

    Product and checkout - buy button

--- src/styles/pages/product.ts
+++ src/styles/pages/product.ts
@@ -50,7 +50,7 @@ export const ProductDetails = styled('div', {
     color: '$gray300',
   },
 
-  botton: {
+  button: {
     marginTop: 'auto',
     backgroundColor: '$green500',
     border: 0,
@@ -61,7 +61,12 @@ export const ProductDetails = styled('div', {
     fontWeight: 'bold',
     fontSize: '$md',
 
-    '&:hover': {
+    '&:disabled': {
+      opacity: 0.6,
+      cursor: 'not-allowed',
+    },
+
+    '&:not(:disabled):hover': {
       backgroundColor: '$green300',
     }
   }
